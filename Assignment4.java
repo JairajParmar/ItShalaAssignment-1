2236ce55b34297496745d59d5c850041953c946f
Jairaj Parmar <jairajsinghparmar26@gmail.com>
2023-03-10 15:38:31 +0530

    Assignments

---
+++ Assignment4.java
@@ -0,0 +1,10 @@
+public class Assignment4 {
+    public static void main(String[] args) {
+        int number1 = 2;
+        int number2 = 4;
+        int number3 = 5;
+        int sum = number1 + number2 + number3;
+        System.out.println("Sum Of Number" + sum);
+
+    }
+}
---
+++ Assignment5.java
@@ -0,0 +1,7 @@
+public class Assignment5 {
+    public static void main(String[] args) {
+        int number1 = 45;
+        int number2 = 32;
+        System.out.println("Multiplicaton of Number=" + (number1 * number2));
+    }
+}
